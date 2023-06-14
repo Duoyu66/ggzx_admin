bf3324071e477789ace1d9d11df99427f81d305c
lbl <libl@qixiangyun.com>
2023-06-14 20:27:05 +0800

    login模块实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,23 +3,47 @@
 </script>
 
 <template>
-<div class="login_container">
+  <div class="login_container">
+    <el-row>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>欢迎来到硅谷甄选!</h2>
+          <el-form-item>
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input type="password"></el-input>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
 
-
-</div>
+  </div>
 </template>
 
 <style scoped>
-.login_container{
+.login_container {
   width: 100%;
   height: 100vh;
 
 
-  //background-image: url("../asset/image.background.jpg");
-  background-color: red;
+  background: url('@/assets/image/background.jpg') no-repeat;
+  background-size: cover;
+//background-color: red;
 
 
-
-
+  .login_form {
+    position: relative;
+    width: 80%;
+    top: 30vh;
+    background: url("@/assets/image/login_form.png");
+    background-size: cover;
+    h1 {
+      color: white;
+      font-size: 40px;
+    }
+  }
 }
 </style>
